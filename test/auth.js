cd053c2fd8cb45fed29f79593a38ecbf32929784
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-02-10 17:27:47 -0800

    Source formatting

--- test/auth.js
+++ test/auth.js
@@ -4,7 +4,7 @@ const http = require('http');
 const request = require('supertest');
 const wedeployMiddleware = require('../');
 
-describe('auth()', function() {
+describe('.auth()', function() {
   it('should export auth function', function() {
     assert(typeof wedeployMiddleware.auth, 'function');
   });
@@ -26,185 +26,188 @@ describe('auth()', function() {
       wedeployMiddleware.auth({url: ''});
     });
   });
+});
 
-  describe('cookies', function() {
-    let server;
-    before(function() {
-      server = createServer().listen(8888);
-    });
-
-    after(function(done) {
-      server.close(() => done());
-    });
-
-    it('should respond as authorized if token present in cookies', function(done) {
-      request(server)
-        .get('/')
-        .set('Cookie', ['access_token=token'])
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as authorized if token present in cookies with multiple values', function(done) {
-      request(server)
-        .get('/')
-        .set('Cookie', ['access_token=token', 'access_token=wrong', 'foo=bar'])
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as unauthorized if token not present in cookies', function(done) {
-      request(server)
-        .get('/')
-        .set('Cookie', ['foo=bar'])
-        .end((err, res) => {
-          assert.strictEqual(401, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as authorized if token present in querystring', function(done) {
-      request(server)
-        .get('/?access_token=token')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as authorized if token present in headers (Bearer)', function(done) {
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer token')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as authorized if token present in headers (Basic)', function(done) {
-      request(server)
-        .get('/')
-        .set('Authorization', 'Basic dXNlcjpwYXNz')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as unauthorized if token not present in headers, cookies or querystring', function(done) {
-      request(server)
-        .get('/')
-        .end((err, res) => {
-          assert.strictEqual(401, res.statusCode);
-          done();
-        });
-    });
-
-    it('should respond as authorized if unknown authorization scheme', function(done) {
-      request(server)
-        .get('/')
-        .set('Authorization', 'Unknown token')
-        .end((err, res) => {
-          assert.strictEqual(401, res.statusCode);
-          done();
-        });
-    });
+describe('cookies', function() {
+  it('should respond as authorized if token present in cookies', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Cookie', ['access_token=token'])
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
   });
 
-  describe('redirect', function() {
-    it('should redirect if token not present in headers, cookies or querystring', function(done) {
-      let server = createServer('/login').listen(8888);
-      request(server)
-        .get('/')
-        .end((err, res) => {
-          assert.strictEqual(302, res.statusCode);
-          server.close(() => done());
-        });
-    });
-
-    it('should redirect if unauthorized', function(done) {
-      let server = createServer('/login', true).listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(302, res.statusCode);
-          server.close(() => done());
-        });
-    });
+  it('should respond as authorized if token present in cookies with multiple values', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Cookie', ['access_token=token', 'access_token=wrong', 'foo=bar'])
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
   });
 
-  describe('scopes', function() {
-    it('should redirect if scope is invalid', function(done) {
-      let server = createServer('/login', false, ['invalidScope']).listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(302, res.statusCode);
-          server.close(() => done());
-        });
-    });
+  it('should respond as unauthorized if token not present in cookies', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Cookie', ['foo=bar'])
+      .end((err, res) => {
+        assert.strictEqual(401, res.statusCode);
+        server.close(() => done());
+      });
+  });
+});
 
-    it('should not redirect if scope is valid', function(done) {
-      let server = createServer('/login', false, ['validScope']).listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          server.close(() => done());
-        });
-    });
+describe('querystring', function() {
+  it('should respond as authorized if token present in querystring', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/?access_token=token')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
+});
 
-    it('should not redirect if scope is not specified', function(done) {
-      let server = createServer('/login').listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          server.close(() => done());
-        });
-    });
+describe('headers', function() {
+  it('should respond as authorized if token present in headers (Bearer)', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer token')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
 
-    it('should not authorize if scope is invalid', function(done) {
-      let server = createServer(null, false, ['invalidScope']).listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(401, res.statusCode);
-          server.close(() => done());
-        });
-    });
+  it('should respond as authorized if token present in headers (Basic)', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Basic dXNlcjpwYXNz')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
 
-    it('should authorize if scope is valid', function(done) {
-      let server = createServer(null, false, ['validScope']).listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          server.close(() => done());
-        });
-    });
+  it('should respond as authorized if unknown authorization scheme', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Unknown token')
+      .end((err, res) => {
+        assert.strictEqual(401, res.statusCode);
+        server.close(() => done());
+      });
+  });
 
-    it('should authorize if scope is not specified', function(done) {
-      let server = createServer().listen(8888);
-      request(server)
-        .get('/')
-        .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
-        .end((err, res) => {
-          assert.strictEqual(200, res.statusCode);
-          server.close(() => done());
-        });
-    });
+  it('should redirect if unauthorized', function(done) {
+    let server = createServer('/login', true).listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(302, res.statusCode);
+        server.close(() => done());
+      });
+  });
+});
+
+describe('token presence', function() {
+  it('should respond as unauthorized if token not present in headers, cookies or querystring', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .end((err, res) => {
+        assert.strictEqual(401, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should redirect if token not present in headers, cookies or querystring', function(done) {
+    let server = createServer('/login').listen(8888);
+    request(server)
+      .get('/')
+      .end((err, res) => {
+        assert.strictEqual(302, res.statusCode);
+        server.close(() => done());
+      });
+  });
+});
+
+describe('scopes', function() {
+  it('should redirect if scope is invalid', function(done) {
+    let server = createServer('/login', false, ['invalidScope']).listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(302, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should not redirect if scope is valid', function(done) {
+    let server = createServer('/login', false, ['validScope']).listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should not redirect if scope is not specified', function(done) {
+    let server = createServer('/login').listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should not authorize if scope is invalid', function(done) {
+    let server = createServer(null, false, ['invalidScope']).listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(401, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should authorize if scope is valid', function(done) {
+    let server = createServer(null, false, ['validScope']).listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
+  });
+
+  it('should authorize if scope is not specified', function(done) {
+    let server = createServer().listen(8888);
+    request(server)
+      .get('/')
+      .set('Authorization', 'Bearer skipRedirectBecauseTokenWasMissing')
+      .end((err, res) => {
+        assert.strictEqual(200, res.statusCode);
+        server.close(() => done());
+      });
   });
 });
 
